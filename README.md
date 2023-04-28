d3ce0750616fb20f67ea64da4b01e392740352d7
huangduirong <huangduirong@huawei.com>
2023-04-27 23:53:43 -0400

    Init ifm_libgcrypt soruce.

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ nettle官方网站：http://www.lysator.liu.se/~nisse/nettle/
 
 1.  执行如下命令安装gmock-devel cmake make
 ```
-yum install -y gmock-devel cmake make gcc-c++
+yum install -y gmock-devel cmake make gcc-c++ nettle-devel libgcrypt-devel
 ```
 2.  使用如下命令clone代码
 ```
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,15 +1,22 @@
 message("${BoldGreen}---- Selected options begin ----${ColourReset}")
 
-option(ENABLE_GCOV "set lcr gcov option" OFF)
+option(ENABLE_GCOV "set ifm_nettle gcov option" OFF)
 if (ENABLE_GCOV STREQUAL "ON")
     message("${Green}--  Enable gcov${ColourReset}")
 endif()
 
-option(ENABLE_LIBIFM_NETTLE "enable lcr liblcr option" ON)
+option(ENABLE_LIBIFM_NETTLE "enable ifm_nettle libifm_nettle option" ON)
 if (ENABLE_LIBIFM_NETTLE STREQUAL "ON")
     add_definitions(-DENABLE_LIBIFM_NETTLE=1)
     set(ENABLE_LIBIFM_NETTLE 1)
-    message("${Green}--  Enable liblcr${ColourReset}")
+    message("${Green}--  Enable libifm_nettle${ColourReset}")
+endif()
+
+option(ENABLE_LIBIFM_LIBGCRYPT "enable ifm_libgcrypt libifm_libgcrypt option" ON)
+if (ENABLE_LIBIFM_LIBGCRYPT STREQUAL "ON")
+    add_definitions(-DENABLE_LIBIFM_LIBGCRYPT=1)
+    set(ENABLE_LIBIFM_LIBGCRYPT 1)
+    message("${Green}--  Enable libifm_libgcrypt${ColourReset}")
 endif()
 
 message("${BoldGreen}---- Selected options end ----${ColourReset}")
